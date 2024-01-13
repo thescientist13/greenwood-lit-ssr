670592e0782b42729094e8a05a607594bfb71414
Owen Buckley <owenbuckley13@gmail.com>
2024-01-13 14:09:22 -0500

    basic lit hydration support so things at least work

--- src/pages/products.js
+++ src/pages/products.js
@@ -2,8 +2,33 @@
 // import { html } from '@lit-labs/ssr';
 import { LitElement, html } from 'lit';
 import '../components/card.js';
-// import { unsafeStatic, withStatic } from 'lit/static-html.js';
 import { getProducts } from '../services/products.js';
+
+export const hydration = true;
+
+export async function loader(request = null) {
+  const products = await getProducts();
+
+  return { products };
+}
+
+export function getBody(compilation, page, data) {
+  const { products } = data;
+
+  return html`
+    ${
+      products.map((product, idx) => {
+        const { title, thumbnail } = product;
+        return html`
+          <app-card
+            title="${idx + 1}) ${title}"
+            thumbnail="${thumbnail}"
+          ></app-card>
+        `;
+      })
+    }
+  `;
+}
 
 // export default class ProductsPage extends LitElement {
 
@@ -41,29 +66,3 @@ import { getProducts } from '../services/products.js';
 // // for now these are needed for the Lit specific implementations
 // customElements.define('products-page', ProductsPage);
 // export const tagName = 'products-page';
-
-export function getBody(compilation, page, data) {
-  const { products } = data;
-
-  return html`
-    ${
-      products.map((product, idx) => {
-        const { title, thumbnail } = product;
-        return html`
-          <app-card
-            title="${idx + 1}) ${title}"
-            thumbnail="${thumbnail}"
-          ></app-card>
-        `;
-      })
-    }
-  `;
-}
-
-export async function loader(request = null) {
-  const products = await getProducts();
-
-  return { products };
-}
-
-export const hydration = true;
