e29184c7ee78e231066384500bac871ee3ac2cf6
Owen Buckley <owenbuckley13@gmail.com>
2024-02-26 21:46:20 -0500

    API route isolation patching

--- src/api/fragment.js
+++ src/api/fragment.js
@@ -1,16 +1,18 @@
 import { render } from '@lit-labs/ssr';
-import { collectResultSync } from '@lit-labs/ssr/lib/render-result.js'
+import { collectResult } from '@lit-labs/ssr/lib/render-result.js'
 import { html } from 'lit';
 import { unsafeHTML } from 'lit/directives/unsafe-html.js';
 import { getProducts } from '../services/products.js';
 import '../components/card.js';
+
+export const isolation = true;
 
 export async function handler(request) {
   const params = new URLSearchParams(request.url.slice(request.url.indexOf('?')));
   const limit = params.has('limit') ? parseInt(params.get('limit'), 10) : 5;
   const offset = params.has('offset') ? parseInt(params.get('offset'), 10) : 0;
   const products = (await getProducts()).slice(offset, offset + limit);
-  const body = collectResultSync(render(html`
+  const body = await collectResult(render(html`
     ${
       unsafeHTML(products.map((item, idx) => {
         const { title, thumbnail } = item;
--- src/api/search.js
+++ src/api/search.js
@@ -1,9 +1,11 @@
 import { render } from '@lit-labs/ssr';
-import { collectResultSync } from '@lit-labs/ssr/lib/render-result.js'
+import { collectResult } from '@lit-labs/ssr/lib/render-result.js'
 import { html } from 'lit';
 import { unsafeHTML } from 'lit/directives/unsafe-html.js';
 import { getProducts } from '../services/products.js';
 import '../components/card.js';
+
+export const isolation = true;
 
 export async function handler(request) {
   const formData = await request.formData();
@@ -17,7 +19,7 @@ export async function handler(request) {
   if (products.length === 0) {
     body = 'No results found.';
   } else {
-    body = collectResultSync(render(html`
+    body = await collectResult(render(html`
       ${
         unsafeHTML(products.map((item, idx) => {
           const { title, thumbnail } = item;
