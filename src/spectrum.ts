05cf8c5107143e6a97991dc001d55de165529e2e
Owen Buckley <owenbuckley13@gmail.com>
2024-11-27 12:58:44 -0500

    latest patches and demo

--- src/spectrum.ts
+++ src/spectrum.ts
@@ -1,3 +1,4 @@
+// https://opensource.adobe.com/spectrum-web-components/tools/theme/#advanced-usage
 import '@spectrum-web-components/theme/sp-theme.js';
 import '@spectrum-web-components/theme/src/themes.js';
 
@@ -7,3 +8,7 @@ import '@spectrum-web-components/menu/sp-menu.js';
 import '@spectrum-web-components/menu/sp-menu-item.js';
 import '@spectrum-web-components/menu/sp-menu-divider.js';
 import '@spectrum-web-components/menu/sp-menu-group.js';
+
+import SpectrumCard from '@spectrum-css/card' with { type: 'css' };
+
+console.log({ SpectrumCard });
