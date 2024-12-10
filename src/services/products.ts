ad8b48d32fccb955ae946aafbdf6965aa03741a9
Owen Buckley <owenbuckley13@gmail.com>
2024-12-09 20:35:30 -0500

    adopt typescript (#25)

--- src/services/products.ts
+++ src/services/products.ts
@@ -1,4 +1,9 @@
-async function getProducts() {
+type Product = {
+  title: string,
+  thumbnail: string
+}
+
+async function getProducts(): Promise<[Product]> {
   const data = (await fetch(`https://dummyjson.com/products`)
     .then(resp => resp.json()));
   const { products } = data;
