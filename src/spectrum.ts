4bfdbad0520cca66106e2f0fab481a69f339a116
Owen Buckley <owenbuckley13@gmail.com>
2025-02-06 17:27:08 -0500

    handle TS errors

--- src/spectrum.ts
+++ src/spectrum.ts
@@ -9,7 +9,9 @@ import '@spectrum-web-components/menu/sp-menu-item.js';
 import '@spectrum-web-components/menu/sp-menu-divider.js';
 import '@spectrum-web-components/menu/sp-menu-group.js';
 
+// @ts-expect-error see https://github.com/jianliao/spectrum-css/issues/64
 import SpectrumCard from '@spectrum-css/card' with { type: 'css' };
+// @ts-expect-error see https://github.com/jianliao/spectrum-css/issues/64
 import SpectrumTokens from "@spectrum-css/tokens" with { type: "css" };
 
 console.log({ SpectrumCard, SpectrumTokens });
