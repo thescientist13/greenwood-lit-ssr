c67d92d89632d34b0038ec71b2e665bf1040157d
Owen Buckley <owenbuckley13@gmail.com>
2025-03-13 11:43:39 -0400

    import attributes example

---
+++ src/components/header/header.ts
@@ -0,0 +1,32 @@
+import { html, LitElement } from "lit";
+import { unsafeHTML } from 'lit/directives/unsafe-html.js';
+import nav from "./nav.json" with { type: "json" };
+
+// CSSStyleSheet is not supported by Lit
+// https://github.com/lit/lit/issues/2631#issuecomment-1065400805
+// import sheet from './header.css' with { type: 'css' };
+
+class HeaderComponent extends LitElement {
+  // static styles = [sheet];
+
+  render() {
+    return html`
+      <style>
+      :host {
+        header {
+          color: red;
+        }
+      }
+      </style>
+      <header>
+        <nav>
+          <ul>
+            ${nav.items.map((item) => unsafeHTML(`<li>${item}</li>`))}
+          </ul>
+        </nav>
+      </header>
+    `;
+  }
+}
+
+customElements.define("app-header", HeaderComponent);
