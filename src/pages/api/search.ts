203b363f7b1adba60f231bb0e2a877a44c026340
Owen Buckley <owenbuckley13@gmail.com>
2026-06-26 20:02:04 -0400

    feat: demo web awesome ssr

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -5,6 +5,8 @@ import { html } from 'lit';
 import { unsafeHTML } from 'lit/directives/unsafe-html.js';
 import { getProducts } from '../../services/products.ts';
 import '../../components/card/card.ts';
+// import this specifically on the backend to avoid duplicate define calls when importing catd.ts
+import '@awesome.me/webawesome/dist/components/button/button.js';
 
 export const isolation = true;
 
