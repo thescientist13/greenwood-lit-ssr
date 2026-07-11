3db760f518522cd4f83472d689902b7e0b4bef82
Owen Buckley <owenbuckley13@gmail.com>
2026-07-11 19:24:19 -0400

    feat: enable import attributes css polyfill

--- greenwood.config.ts
+++ greenwood.config.ts
@@ -4,6 +4,11 @@ import { greenwoodPluginRendererLit } from '@greenwood/plugin-renderer-lit';
 
 const config: Config = {
   useTsc: true,
+  // enable this polyfill until there is Safari support for CSS Module Scripts
+  // https://github.com/ProjectEvergreen/greenwood/discussions/1275
+  polyfills: {
+    importAttributes: ['css']
+  },
   plugins: [
     greenwoodPluginRendererLit(),
     greenwoodPluginAdapterVercel({
