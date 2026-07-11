1c103614a34e049051464903f77ae80c8346a141
Owen Buckley <owenbuckley13@gmail.com>
2026-07-11 09:36:24 -0400

    feat: dynamic routing example (#42)

---
+++ src/pages/product/[id].ts
@@ -0,0 +1,20 @@
+import { html } from "lit";
+import { getProductById } from '../../services/products.ts';
+import '../../components/card/card.ts';
+import type { Compilation, Page } from "@greenwood/cli";
+
+type Params = {
+  id: string
+}
+
+export async function getBody(compilation: Compilation, page: Page, request: Request, params: Params) {
+  const product = await getProductById(parseInt(params.id, 10));
+  const { title, thumbnail } = product;
+
+  return html`
+    <app-card
+      title="${title}"
+      thumbnail="${thumbnail}"
+    ></app-card>
+  `;
+}
